53b1441eaf3f66e681b7bb3c148a9fa28a492bc2
GKNSB <manoshatzis@gmail.com>
2024-07-16 14:59:30 +0300

    Update Azure.py

--- takeovers/Azure.py
+++ takeovers/Azure.py
@@ -7,7 +7,7 @@ def init(domain, ARecords, CNAMERecords):
 	for entry in CNAMERecords:
 		CNAME = str(entry)[:-1]
 
-		if any(azureSub in CNAME for azureSub in ["azure-api.net", "azurecontainer.io", "azurecr.io", "azuredatalakeanalytics.net", "azuredatalakestore.net", 
+		if any(azureSub in CNAME for azureSub in ["azure-api.net", "azurecontainer.io", "azurecr.io", "azuredatalakeanalytics.net", "azuredatalakestore.net", "azurehealthcareapis.com", 
 			"azureedge.net", "azurehdinsight.net", "azurefd.net", "azurehealthcareapis.com", "azureiotcentral.com", "azurewebsites.net", "batch.azure.com",
 			"blob.core.windows.net", "cloudapp.azure.com", "cloudapp.net", "core.windows.net", "database.windows.net", "p.azurewebsites.net", "redis.cache.windows.net", 
 			"search.windows.net", "service.signalr.net", "servicebus.windows.net", "trafficmanager.net", "visualstudio.com"]):
